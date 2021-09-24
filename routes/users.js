8e4cee39e439a54b00f4ec12d50eac6144a03e33
gbrozdev <gbrozdev@gmail.com>
2021-09-25 02:55:02 +0530

    Project Initial Release

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function(req, res) {
+  res.render('admin');
+});
+
+router.get('/deletAll', function(req, res) {
+  db.get().collection('lyrics').remove()
+  res.redirect('/');
 });
 
 module.exports = router;
