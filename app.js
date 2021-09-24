3e380cadf46f1de11512c67b9e99dd1763cd27e7
gbrozdev <gbrozdev@gmail.com>
2021-09-24 16:06:36 +0530

    Lyrica Deployed

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if(process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -6,12 +10,13 @@ var logger = require('morgan');
 var db = require('./connection');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+const hbs = require('express-handlebars');
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+app.engine('hbs',hbs({extname:'hbs',defaultLayout:'layout',layoutsDir:__dirname + '/views/layout/',partialsDir:__dirname + '/views/partials/'}))
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,10 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-db.connect((err)=>{
-  if(err) console.log("Connection Error"+err);
-  else console.log("Database connected to port")
-})
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -31,6 +33,11 @@ app.use('/users', usersRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+db.connect((err)=>{
+  if(err) console.log("Connection Error"+err);
+  else console.log("Database connected to port")
+})
 
 // error handler
 app.use(function(err, req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
-
+var db = require('../connection')
+var ObjectId = require('mongodb').ObjectId
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+router.get('/', async function(req, res) {
+    var lyrics = await db.get().collection('lyrics').find().toArray()
+    console.log(lyrics);
+    res.render('index');
+});
+
+router.get('/add-lyrics', function(req, res) {
+    res.render('add-lyrics');
+});
+
+router.get('/lyrics',async function(req, res) {
+    var lyrics = await db.get().collection('lyrics').find().toArray()
+    console.log(lyrics);
+    res.render('lyrics' , {lyrics});
+});
+
+router.get('/lyrica/:id',async function(req, res) {
+    var id = req.params.id
+    console.log(id);
+    var lyrica = await db.get().collection('lyrics').findOne({_id:ObjectId(id)})
+    console.log(lyrica);
+    res.render('lyrica',{lyrica});
+});
+
+router.post('/add-lyrics', function(req, res) {
+    var link = req.body.link
+    var newlink = link.replace("https://youtu.be/", "");
+    req.body.link = newlink
+    db.get().collection('lyrics').insertOne(req.body)
+    res.redirect('/')
 });
 
 module.exports = router;
